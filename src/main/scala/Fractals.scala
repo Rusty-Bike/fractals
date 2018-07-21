ab0b4576aa97a80818a67e761f3f065d5442ac98
Andrew Smith <smith.andrew.l@gmail.com>
2018-07-20 22:02:04 -0500

    Added animation to the drawing.

--- src/main/scala/Fractals.scala
+++ src/main/scala/Fractals.scala
@@ -1,17 +1,62 @@
+import Fractals.{lines, renderer}
 import sdl2.SDL._
 import sdl2.Extras._
+import sdl2.SDL
 
 import scala.scalanative.native._
 
 object Fractals extends SdlApp(c"Fractals", 800, 800) with App {
 
+  class Animator(var lines: List[LineSegment]) {
+    val backgroundColor = (0, 0, 0)
+    val lineColor = (255, 255, 255)
+
+
+    var currentStep = 1
+    val timeToWait = 1
+    var lastLineDrawTime: Word = System.currentTimeMillis()
+
+    def reset(): Unit = {
+      currentStep = 1
+    }
+    def draw( renderer: Ptr[SDL.SDL_Renderer]): Unit = {
+      // Set the draw color
+      // clear the screen
+      // Set the line draw color
+
+      SDL_SetRenderDrawColor(renderer, 0.toUByte, 0.toUByte, 0.toUByte, SDL_ALPHA_OPAQUE)
+
+      SDL_RenderClear(renderer)
+
+      SDL_SetRenderDrawColor(renderer, 255.toUByte, 255.toUByte, 255.toUByte, SDL_ALPHA_OPAQUE)
+
+      if(currentStep == lines.length) {
+        currentStep = 1
+      }
+
+      val timeNow = System.currentTimeMillis()
+      val drawNextLine = (timeNow - lastLineDrawTime) > timeToWait
+      if(drawNextLine) {
+        currentStep = currentStep + 1
+        lastLineDrawTime = timeNow
+      }
+
+      lines.view.take(currentStep).foreach(linesegment => SDL_RenderDrawLine(renderer, linesegment.start.x, linesegment.start.y, linesegment.end.x, linesegment.end.y))
+
+    }
+
+  }
+
+  var animator: Animator =  _
   type Fractal = (Int, Int, Int, Point) => List[LineSegment]
   
   var currentFractal: Fractal = sierpinski
   
   var lines: List[LineSegment] = _
   var depth: Int = 0
-  
+
+
+
   case class Point(val x: Int, val y: Int) {
   	def incX(amount: Int): Point = {
   		copy(x = x + amount)
@@ -35,8 +80,7 @@ object Fractals extends SdlApp(c"Fractals", 800, 800) with App {
   	def toLines(): List[LineSegment] = {
       val topPoint         = new Point(bottomLeftPoint.x + (length / 2), bottomLeftPoint.y - (length))
       val bottomRightPoint = new Point(bottomLeftPoint.x + length,       bottomLeftPoint.y)
-      
-      
+
       val leftLine   = new LineSegment(bottomLeftPoint,  topPoint)
       val rightLine  = new LineSegment(bottomRightPoint, topPoint)
       val bottomLine = new LineSegment(bottomLeftPoint,  bottomRightPoint)
@@ -44,7 +88,7 @@ object Fractals extends SdlApp(c"Fractals", 800, 800) with App {
       List(bottomLine, leftLine, rightLine)
   	}
   }
-  
+
   def sierpinski(currentDepth: Int, iterations: Int, length: Int, bottomLeftPoint: Point): List[LineSegment] = {
     val triangle = Triangle(bottomLeftPoint, length)
 
@@ -67,7 +111,8 @@ object Fractals extends SdlApp(c"Fractals", 800, 800) with App {
 
 
   override def main(args: Array[String]): Unit = {
-    lines = currentFractal(0, 0, 800, new Point(0, 799))
+    lines = currentFractal(0, depth, 800, new Point(0, 799))
+    animator = new Animator(lines)
     super.main(args)
   }
 
@@ -77,8 +122,11 @@ object Fractals extends SdlApp(c"Fractals", 800, 800) with App {
         SDL_Quit()
         System.exit(0)
       case SDL_KEYUP => {
-        depth = ((depth + 1) % 8)
+        depth =  (depth  + 1) % 8
+        lines = List()
         lines = currentFractal(0, depth, 800, new Point(0, 799))
+        animator.lines = lines
+        animator.reset()
       }
       case _ =>
         ()
@@ -86,14 +134,7 @@ object Fractals extends SdlApp(c"Fractals", 800, 800) with App {
   }
 
   override def onDraw(): Unit = {
-    SDL_SetRenderDrawColor(renderer, 0.toUByte, 0.toUByte, 0.toUByte, SDL_ALPHA_OPAQUE)
-  
-    SDL_RenderClear(renderer)
-    
-    SDL_SetRenderDrawColor(renderer, 255.toUByte, 255.toUByte, 255.toUByte, SDL_ALPHA_OPAQUE)
-
-    lines.foreach(linesegment => SDL_RenderDrawLine(renderer, linesegment.start.x, linesegment.start.y, linesegment.end.x, linesegment.end.y))
-
+    animator.draw(renderer)
     SDL_RenderPresent(renderer)
   }
 
