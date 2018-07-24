338dec55ee7478df650619de5a1f45c07b91f45b
Andrew Smith <smith.andrew.l@gmail.com>
2018-07-23 19:18:21 -0500

    Code cleanup.

--- src/main/scala/FractalsApp.scala
+++ src/main/scala/FractalsApp.scala
@@ -1,26 +1,19 @@
 import DrawingPrimitives.{LineSegment, Point}
 import Fractals.{Fractal, sierpinski, vicsek, vicsekx}
-import FractalsApp.FractalImpl.FractalImpl
 import sdl2.Extras._
 import sdl2.SDL._
 
 import scala.scalanative.native._
 
 object FractalsApp extends SdlApp(c"Fractals", 800, 800) with App {
-
-  object FractalImpl extends Enumeration {
-    type FractalImpl = Value
-    val Sierpinski, Vicsek, VicsekX = Value
-  }
-
-  var fractals: Array[Fractal] = _
-  var animator:       Animator =  _
-  var currentFractal: Int = 0
-  var depth:          Int      =  0
+  var fractals:       Array[Fractal] = _
+  var animator:       Animator       = _
+  var currentFractal: Int            = 0
+  var depth:          Int            = 0
 
   override def main(args: Array[String]): Unit = {
-    fractals= Array(sierpinski, vicsek, vicsekx)
-    animator = new Animator(drawFractal(0, depth, 800, new Point(0, 799)))
+    fractals = Array(sierpinski, vicsek, vicsekx)
+    animator = new Animator(getLinesOfCurrentFractal(0, depth, 800, new Point(0, 799)))
 
     super.main(args)
   }
@@ -36,12 +29,12 @@ object FractalsApp extends SdlApp(c"Fractals", 800, 800) with App {
         if(event.button.button == SDL_BUTTON_LEFT) {
           depth = (depth + 1) % 8
 
-          animator.lines = drawFractal(0, depth, 800, new Point(0, 799))
+          animator.lines = getLinesOfCurrentFractal(0, depth, 800, new Point(0, 799))
           animator.reset()
         } else if(event.button.button == SDL_BUTTON_RIGHT) {
           currentFractal = (currentFractal + 1) % fractals.length
 
-          animator.lines = drawFractal(0, depth, 800, new Point(0, 799))
+          animator.lines = getLinesOfCurrentFractal(0, depth, 800, new Point(0, 799))
           animator.reset()
 
         }
@@ -51,9 +44,8 @@ object FractalsApp extends SdlApp(c"Fractals", 800, 800) with App {
     }
   }
 
-  def drawFractal(currentDepth: Int, maxDepth: Int, width: Int, bottomLeftPoint: Point): List[LineSegment] = {
+  def getLinesOfCurrentFractal(currentDepth: Int, maxDepth: Int, width: Int, bottomLeftPoint: Point): List[LineSegment] = {
         fractals(currentFractal)(currentDepth, maxDepth, width, bottomLeftPoint)
-
   }
   override def onDraw(): Unit = {
     animator.draw(renderer)
