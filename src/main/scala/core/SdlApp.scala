b604b30a36c72e19b6e982c03d6615998c9bea40
giusdp <depalma.gsp@gmail.com>
2018-10-14 11:15:14 +0200

    Initialization of SDL_TTF

--- src/main/scala/core/SdlApp.scala
+++ src/main/scala/core/SdlApp.scala
@@ -9,12 +9,20 @@ import scala.scalanative.native.{Ptr, UInt, stackalloc, _}
 abstract class SdlApp(title: CString, height: Int, width: Int) extends App {
   var window:   Ptr[SDL_Window]   = _
   var renderer: Ptr[SDL_Renderer] = _
+  var font: Ptr[TTF_Font] = _
 
   def init(): Unit = {
 
     SDL_Init(SDL_INIT_VIDEO)
     window   = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN)
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC)
+
+    // If TTF fails to load, the program exites. But we could let it go on silently without showing any fonts.
+    if(TTF_Init() != 0) {
+      println("TTF_Init: %s".format(fromCString(SDL_GetError())))
+      sys.exit(-1)
+    }
+    
   }
 
   def onEvent(event: Ptr[SDL_Event]): Unit
@@ -41,6 +49,8 @@ abstract class SdlApp(title: CString, height: Int, width: Int) extends App {
   def onCleanup(): Unit
 
   def cleanup(): Unit = {
+    if(font != null) { TTF_CloseFont(font) }
+    TTF_Quit()
     SDL_DestroyRenderer(renderer)
     SDL_DestroyWindow(window)
     onCleanup()
