0f324d5ce1f3acd1b658c92261fa0b677f110363
Dzmitryi Linou <dmitry.linov.4gplus@gmail.com>
2019-10-30 21:06:16 +0300

    #1 Add Cesaro fractal

--- src/main/scala/fractals/FractalSegment.scala
+++ src/main/scala/fractals/FractalSegment.scala
@@ -20,16 +20,17 @@ sealed trait FractalSegment {
   * One of the segments of the Koch Curve: _/\_
   * Has direction so that when it divides, it grows new segments in the right places
   */
-final case class KochSegment(start: Vec2, dir: Vec2, length: Double) extends FractalSegment {
+final case class KochSegment(start: Vec2, dir: Vec2, length: Double, degrees: Double) extends FractalSegment {
 
   // Divide into 4 segments _/\_
   def divide: Seq[KochSegment] = {
-    val newLength = length / 3
+    val cos =  Math.cos(Math.toRadians(degrees))
+    val newLength = length / (2 * (1 + cos))
     Seq(
-      KochSegment(start, dir, newLength),
-      KochSegment(start + dir * newLength, dir.rotate(-60), newLength),
-      KochSegment(start + dir * newLength + dir.rotate(-60) * newLength, dir.rotate(60), newLength),
-      KochSegment(start + dir * (newLength * 2), dir, newLength)
+      KochSegment(start, dir, newLength, degrees),
+      KochSegment(start + dir * newLength, dir.rotate(-degrees), newLength, degrees),
+      KochSegment(start + dir * newLength + dir.rotate(-degrees) * newLength, dir.rotate(degrees), newLength, degrees),
+      KochSegment(start + dir * newLength  * (1 + 2 * cos), dir, newLength, degrees)
     )
   }
 }
