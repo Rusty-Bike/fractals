05cca7a0b554ca872be8a0dc3fcd4bb531a66199
Andrew Smith <smith.andrew.l@gmail.com>
2020-10-26 19:54:08 -0500

    Added documentation to Drawing.scala

--- src/main/scala/core/Drawing.scala
+++ src/main/scala/core/Drawing.scala
@@ -2,16 +2,14 @@ package core
 
 import core.DrawingPrimitives.{LineSegment, Point}
 
-/**
- *
- */
 object Drawing {
 
   /**
+   * A Turtle graphics implementation, which renders to a list of line segments
    *
-   * @param location
-   * @param heading
-   * @param path
+   * @param location The starting location
+   * @param heading  The direction that the turtle is facing
+   * @param path     The path that the turtle has taken so far
    */
   case class Turtle(location: Point, heading: Int, path: List[LineSegment]) {
     def left(amount: Int):         Turtle = copy(heading  = heading - amount)
@@ -20,9 +18,12 @@ object Drawing {
     def setHeading(heading: Int):  Turtle = copy(heading  = heading)
 
     /**
+     * Moves the turtle forward in whatever direction he is facing, and adds
+     * a line segment of the move to the path.
      *
-     * @param amount
-     * @return
+     * @param amount The number of steps (pixels) to take
+     * @return Returns a copy of the turtle with an updated path including the
+     *         move.
      */
     def forward(amount: Int): Turtle = {
       val xAdj = (amount * Math.cos(Math.toRadians(heading))).toInt
